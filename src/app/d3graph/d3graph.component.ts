d9be8d70ec0c1c0a88df835f468bdcaafc0d37f1
dan <dan_fs@hotmail.com>
2017-11-14 22:27:34 +0000

    format timeline

--- src/app/d3graph/d3graph.component.ts
+++ src/app/d3graph/d3graph.component.ts
@@ -14,6 +14,7 @@ import {
   Transition
 } from 'd3-ng2-service';
 
+import * as $ from 'jquery';
 import * as Timeline from 'd3-timelines';
 
 declare var Timeline: any;
@@ -21,7 +22,8 @@ declare var Timeline: any;
 
 @Component({
   selector: 'app-d3graph',
-  template: ''
+  templateUrl: './d3graph.component.html',
+  styleUrls: ['./d3graph.component.css']
 })
 export class D3graphComponent implements OnInit {
   private d3: D3;
@@ -36,10 +38,10 @@ export class D3graphComponent implements OnInit {
   width: number = 100;
   height: number = 350;
   testData = [{label: "The Prophecies of Merlin (Prose Version)", times: [
-        {"color":"rgba(47,89,97,0.2)", "starting_time": 1355752800000, "ending_time": 1355766900000},
-        {"color":"rgba(47,89,97,0.2)", "starting_time": 1355759900000, "ending_time": 1355774400000}]},
-        {label: "person b", times: [{"starting_time": 1355759910000, "ending_time": 1355761900000}]},
-        {label: "person c", times: [{"starting_time": 1355761910000, "ending_time": 1355763910000}]}
+        {"color":"rgba(47,89,97,0.2)", "starting_time": -17990208000000, "ending_time": -16990208000000},
+        {"color":"rgba(47,89,97,0.2)", "starting_time": -17359142400000, "ending_time": -14834534400000}]},
+        {label: "person b", times: [{"color":"rgba(47,64,89,0.2)", "starting_time": -17359142400000, "ending_time": -16990208000000}]},
+        {label: "person c", times: [{"color":"rgba(89,58,47,0.2)",  "starting_time": -17990208000000, "ending_time": -14834534400000}]}
     ];
 
   ngOnInit() {
@@ -49,7 +51,7 @@ export class D3graphComponent implements OnInit {
             let svg: any;
             let name: string;
             let yVal: number;
-            let colors: any = [];
+            let colors: any = ["rgba(47,89,97,0.2)","rgba(47,64,89,0.2)","rgba(89,58,47,0.2)"];
             let data: {name: string, yVal: number}[] = [];
             let padding: number = 25;
             let xScale: any;
@@ -67,35 +69,32 @@ export class D3graphComponent implements OnInit {
     if (this.parentNativeElement !== null) {
       svg = d3.select(this.parentNativeElement)
           .append('svg')        // create an <svg> element
-          .attr('width', '100%') // set its dimensions
+          .attr('width', '90%') // set its dimensions
           .attr('height', this.height);
 
      var chart = Timeline.timelines()
-          .relativeTime()
           .stack()
+          .beginning(-18834534400000)
+          .ending(-13834534400000)
           .tickFormat({
-            format: function(d) { return d3.timeFormat("%M")(d) },
-            tickTime: d3.timeMinutes,
+            format:  d3.timeFormat("%Y") ,
+            tickTime: d3.timeYears,
             tickInterval: 15,
             tickSize: 15,
           })
-          .margin({left:70, right:30, top:0, bottom:0})
+          .margin({left:200, right:30, top:0, bottom:0})
           .hover(function (d, i, datum) {
           // d is the current rendering object
           // i is the index during d3 rendering
           // datum is the id object
-            // var div = $('#hoverRes');
-            // var colors = chart.colors();
-            // div.find('.coloredDiv').css('background-color', colors(i))
-            // div.find('#name').text(datum.name);
+           console.log("timelineHover", datum.label);
+            var div = $('#hoverRes');
+            var colors = chart.colors();
+            div.find('.coloredDiv').css('background-color', colors(i))
+            div.find('#name').text(datum.label);
           })
           .click(function (d, i, datum, selectedLabel, selectedRect, xVal) {
-            console.log("timelineHover", datum.name);
-            console.log("point", xVal)
-            // $("#assays").append("<div>\
-            // <label>Assay Type:</label><select><option value=\"dnase\">DNAseSeq</option><option value=\"rnase\">RNASeq</option></select> \
-            // <label>Assay Timepoint:</label><span>"+ Math.round(xVal) + "</span> \
-            // </div>");
+            console.log("timelineHover", datum.label);
           });
         
         svg = svg.datum(this.testData).call(chart);
