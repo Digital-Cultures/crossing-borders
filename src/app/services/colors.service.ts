1849a8116cae5c44d0d9b8fbfa561513265cb6a7
dan <dan_fs@hotmail.com>
2017-12-09 12:14:41 -0400

    update colours

--- src/app/services/colors.service.ts
+++ src/app/services/colors.service.ts
@@ -2,9 +2,11 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class ColorsService {
-  private color: string[] = ["rgba(47,89,97,0.2)","rgba(47,64,89,0.2)","rgba(89,58,47,0.2)","rgba(47,89,97,0.2)","rgba(47,64,89,0.2)","rgba(89,58,47,0.2)","rgba(47,89,97,0.2)","rgba(47,64,89,0.2)","rgba(89,58,47,0.2)","rgba(47,89,97,0.2)","rgba(47,64,89,0.2)","rgba(89,58,47,0.2)"];
-  private mapMarker: string[] = ["assets/greenMapIcon.png","assets/blueMapIcon.png","assets/redMapIcon.png","assets/greenMapIcon.png","assets/blueMapIcon.png","assets/redMapIcon.png","assets/greenMapIcon.png","assets/blueMapIcon.png","assets/redMapIcon.png","assets/greenMapIcon.png","assets/blueMapIcon.png","assets/redMapIcon.png"];
+  private color: string[] = ["rgba(27,158,119,0.2)", "rgba(217,95,2,0.2)", "rgba(117,112,179,0.2)", "rgba(231,41,138,0.2)", "rgba(102,166,30,0.2)", "rgba(230,171,2,0.2)", "rgba(166,118,29,0.2)", "rgba(102,102,102,0.2)"];
+  private mapMarker: string[] = ["assets/green.png", "assets/orange.png", "assets/purple.png", "assets/pink.png", "assets/lime.png", "assets/yellow.png", "assets/brown.png", "assets/grey.png"];
   private label: string[] = [];
+  private labelColor: string[] = [];
+  private labelMapMarker: string[] = [];
 
   constructor() { }
 
@@ -12,48 +14,56 @@ export class ColorsService {
     return this.color;
   }
 
-  getColorByID(id:number) {
+  getColorByID(id: number) {
     return this.color[id];
   }
 
-  getColorByLabel(name:string){
-  	var exists = false;
+  getColorByLabel(name: string) {
+    var exists = false;
+    var col;
 
+    for (var k = 0; k < this.label.length; k++) {
 
-    for(var k = 0; k < this.label.length; k++) {
-
-        if (this.label[k] === name) {
-            return this.color[k];
-        } 
+      if (this.label[k] === name) {
+        col = this.labelColor[k];
+        return col;
+      }
     };
 
-	//add new series
-	if (!exists){
-		this.label.push(name);
-		return this.color[this.label.length-1];
-	}
+    //add new series
+    if (!exists) {
+      this.label.push(name);
+
+      var i = this.label.length - 1;
+      var j = Math.floor(this.label.length / this.color.length)
+      var k = i - (j * (this.color.length - 1));
+      col = this.color[k];
+      this.labelColor.push(col);
+      return this.labelColor[this.label.length - 1];
+    }
   }
 
-  getMapMarkerByID(id:number) {
-  	return this.mapMarker[id];
+  getMapMarkerByID(id: number) {
+    return this.mapMarker[id];
   }
 
-  getMarkerByLabel(name:string){
-  	var exists = false;
+  getMarkerByLabel(name: string) {
+    var exists = false;
 
 
-    for(var k = 0; k < this.label.length; k++) {
+    for (var k = 0; k < this.label.length; k++) {
 
-        if (this.label[k] === name) {
-            return this.mapMarker[k];
-        } 
+      if (this.label[k] === name) {
+        return this.mapMarker[k];
+      }
     };
 
-	//add new series
-	if (!exists){
-		this.label.push(name);
-		return this.mapMarker[this.label.length-1];
-	}
+    //add new series
+    if (!exists) {
+      this.label.push(name);
+      var i = Math.floor(this.label.length - 1 / this.color.length - 1);
+      return this.mapMarker[this.label.length - 1];
+    }
   }
 
 }
